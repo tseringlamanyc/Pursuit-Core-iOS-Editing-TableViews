b0eb0cac4dcdad35b88cb728e21fbbb7d6d3c3b5
davidlawrencer <davidlrifkin+github@gmail.com>
2019-08-16 12:32:22 -0400

    Adds add item view controller file

---
+++ EditingTableViews/AddItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddItemViewController.swift
+//  EditingTableViews
+//
+//  Created by David Rifkin on 8/16/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
